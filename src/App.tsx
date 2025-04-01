bdae044833cee44e053765b39103161211fad190
felipe bartolomeu <felipebart1999@gmail.com>
2025-03-31 21:55:21 -0300

    seções criadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Banner from './components/Banner'
 import Header from './components/Header'
 import { GlobalCss } from './styles'
+import ProductList from './components/ProductsList'
 
 const rotas = createBrowserRouter([
   {
     path: '/',
-    element: <Banner />
+    element: (
+      <>
+        <Banner />
+        <ProductList title="Promoções" background="gray" />
+        <ProductList title="Em Breve" background="black" />
+      </>
+    )
   }
 ])
 function App() {
---
+++ src/components/ProductsList/index.tsx
@@ -0,0 +1,49 @@
+import Product from '../Product'
+import { Container, List } from './styles'
+
+export type Props = {
+  title: string
+  background: 'gray' | 'black'
+}
+const ProductList = ({ background, title }: Props) => (
+  <Container background={background}>
+    <div className="container">
+      <h2>{title}</h2>
+      <List>
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placeholder.it/222x250"
+          infos={['-10%', 'R$150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placeholder.it/222x250"
+          infos={['-10%', 'R$150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placeholder.it/222x250"
+          infos={['-10%', 'R$150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+        <Product
+          category="Ação"
+          description="teste"
+          image="//placeholder.it/222x250"
+          infos={['-10%', 'R$150']}
+          system="Windows"
+          title="Nome do jogo"
+        />
+      </List>
+    </div>
+  </Container>
+)
+export default ProductList
